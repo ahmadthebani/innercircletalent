8b7ecec3d31998d2dc81f6fb129ca524d67f4daa
Zakaria bijaddigune <96249501+Zakaria0107@users.noreply.github.com>
2024-04-02 19:50:55 +0000

    hashroute

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route , HashRouter } from "react-router-dom";
 import Contact from './pages/Contact';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename='/'>
+  <HashRouter basename='/'>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/contact" element={<Contact />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 
