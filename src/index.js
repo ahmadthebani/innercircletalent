974230c67f9387b81221b08f2618e15f86ece574
Zakaria bijaddigune <96249501+Zakaria0107@users.noreply.github.com>
2024-04-05 00:03:03 +0000

    routing

--- src/index.js
+++ src/index.js
@@ -3,18 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './pages/App';
 import { BrowserRouter, Routes, Route , HashRouter  } from "react-router-dom";
+
 import Contact from './pages/Contact';
 import Easforfounders from './pages/Easforfounders';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
 root.render(
-  <BrowserRouter basename='/'>
+  <HashRouter basename="/">
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/easforfounders" element={<Easforfounders/>} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 
