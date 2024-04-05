96ec5ba321ef46bb75ab80aa1b90cebbf42a8f71
Zakaria bijaddigune <96249501+Zakaria0107@users.noreply.github.com>
2024-04-05 00:15:17 +0000

    complete page

--- src/pages/Easforfounders.jsx
+++ src/pages/Easforfounders.jsx
@@ -4,6 +4,11 @@ import Footer from '../compenents/Footer'
 import Hero from '../compenents/Easforfounders/Hero'
 import Story from '../compenents/Easforfounders/Story'
 import HiringExperts from '../compenents/Easforfounders/HiringExperts'
+import Condidates from "../compenents/Easforfounders/Condidates";
+import Contact from "../compenents/Easforfounders/Contact";
+import FAQ from "../compenents/Easforfounders/FAQ";
+import ForCompanies from "../compenents/Easforfounders/ForCompanies";
+import Team from "../compenents/Easforfounders/Team";
 
 const Easforfounders = () => {
   return (
@@ -12,6 +17,11 @@ const Easforfounders = () => {
         <Hero/>
         <Story/>
         <HiringExperts/>
+        <ForCompanies/>
+        <Condidates/>
+        <Team/>
+        <FAQ/>
+        <Contact/>
         <Footer/>
     </>
   )
